bf9e071ba80f0326e005105c4c158a0ee49269fc
zeling595 <longzeling595@gmail.com>
2020-05-31 18:22:01 +0800

    commit

---
+++ NUSJio/NUSJio/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  NUSJio
+//
+//  Created by Zeling Long on 2020/5/30.
+//  Copyright © 2020 Zeling Long. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NUSJio/NUSJio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NUSJio
+//
+//  Created by Zeling Long on 2020/5/30.
+//  Copyright © 2020 Zeling Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
