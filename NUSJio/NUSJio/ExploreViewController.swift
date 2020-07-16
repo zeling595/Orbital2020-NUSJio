5a971cbd0c74390478b1e8496c107084433cba37
ChengJiyuqing <chengjiyuqing@gmail.com>
2020-07-16 22:18:48 +0800

    search improved

--- NUSJio/NUSJio/ExploreViewController.swift
+++ NUSJio/NUSJio/ExploreViewController.swift
@@ -13,6 +13,24 @@ import Foundation
 
 class ExploreViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, UISearchBarDelegate {
     
+    class OrderedActivity{
+        var activity: Activity;
+        var priority: Int;
+        
+        init (activity: Activity) {
+            self.activity = activity;
+            priority = 0;
+        }
+        
+        func incrementPriority() {
+            self.priority = self.priority + 1;
+        }
+        
+        func decrementPriority() {
+            self.priority = self.priority - 1;
+        }
+    }
+    
     @IBOutlet var listOfActivities: UITableView!
     @IBOutlet var searchBar: UISearchBar!
     
@@ -24,16 +42,22 @@ class ExploreViewController: UIViewController, UITableViewDelegate, UITableViewD
     var filtered = false;
     
     override func viewWillAppear(_ animated: Bool) {
-        filtered = false;
-        dataController.fetchAllActivities { (activities) in
-            if let activities = activities {
-                print(activities)
-                self.allActivities = activities
-                self.listOfActivities.reloadData()
-            } else {
-                print("error fetching all activities")
+        if !searchBar.text!.isEmpty  {
+            filtered = true;
+            filterActivity(searchBar.text);
+        } else {
+            filtered = false;
+            dataController.fetchAllActivities { (activities) in
+                if let activities = activities {
+                    print(activities)
+                    self.allActivities = activities
+                    self.listOfActivities.reloadData()
+                } else {
+                    print("error fetching all activities")
+                }
             }
         }
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -56,7 +80,12 @@ class ExploreViewController: UIViewController, UITableViewDelegate, UITableViewD
            if segue.identifier == "ExploreToDetailSegue",
                 let exploreDetailController = segue.destination as? ExploreDetailViewController {
                 let indexPath = listOfActivities.indexPathForSelectedRow!
-                let selectedActivity = allActivities[indexPath.row]
+            var selectedActivity : Activity;
+            if filtered {
+                selectedActivity = filteredActivities[indexPath.row]
+            } else {
+                selectedActivity = allActivities[indexPath.row]
+            }
                 exploreDetailController.activity = selectedActivity
                 exploreDetailController.activityIndexPath = indexPath
             }
@@ -120,11 +149,19 @@ class ExploreViewController: UIViewController, UITableViewDelegate, UITableViewD
         dataController.fetchImage(imageURL: activity.imageURLStr, completion: { (imageData) in
             if let imageData = imageData {
                 cell.imageView!.image = UIImage(data: imageData)
-                print("imageData is fetched")
+                //print("imageData is fetched")
             }
         })
         cell.textLabel?.text = activity.title
         cell.detailTextLabel?.text = activity.description;
+    }
+    
+    func compare(a1: OrderedActivity, a2: OrderedActivity) -> Bool{
+        if a1.priority >= a2.priority {
+            return true;
+        } else {
+            return false;
+        }
     }
     
     
@@ -133,23 +170,80 @@ class ExploreViewController: UIViewController, UITableViewDelegate, UITableViewD
         guard let query = query else {return}
         print("searching")
         let text = query.lowercased();
+        let splitedText = text.components(separatedBy: " ");
+        var sortedActivity: [OrderedActivity] = [];
+        
+        var prefixedText : [String] = [];
+        var suffixedText : [String] = [];
+        for text in splitedText {
+            prefixedText.append(String(text.prefix(3)))
+            suffixedText.append(String(text.suffix(3)))
+        }
+        print (prefixedText)
+        print (suffixedText)
+        
         for activity in allActivities {
+            var wrappedActivity = OrderedActivity(activity: activity);
                 let title = activity.title.lowercased()
-                let location = activity.location
+            let location = activity.location?.lowercased()
                 let tags = activity.tags
-                let description = activity.description
+            let description = activity.description?.lowercased()
                 //the real search part:
             
-                if title.contains(text) {
-                    filteredActivities.append(activity);
-                } else if description != nil && description!.lowercased().contains(text) {
-                    filteredActivities.append(activity);
-                } else if tags != nil && tags!.contains(text) {
+            
+            
+            //looping through splited text (i.e. all seperated words)
+            for i in 0...splitedText.count-1 {
+                //if one/some of the words are contained
+                let word = splitedText[i];
+                if title.contains(word) {
+                    sortedActivity.append(wrappedActivity);
+                } else if description != nil && description!.lowercased().contains(word) {
+                    sortedActivity.append(wrappedActivity);
+                } else if tags != nil && tags!.contains(word) {
                     //TODO: make tags lowercase
-                    filteredActivities.append(activity);
-                } else if location != nil && location!.lowercased().contains(text) {
-                    filteredActivities.append(activity);
+                    sortedActivity.append(wrappedActivity);
+                } else if location != nil && location!.lowercased().contains(word) {
+                    //sortedActivity.append(wrappedActivity);
+                } else {
+                    wrappedActivity.decrementPriority();
                 }
+            }
+            //to search prefix/suffix
+            for j in 0...splitedText.count-1 {
+                let word1 = prefixedText[j]
+                let word2 = suffixedText[j]
+                if title.contains(word1) || title.contains(word2){
+                    sortedActivity.append(wrappedActivity);
+                } else if description != nil && (description!.lowercased().contains(word1) || description!.lowercased().contains(word2)) {
+                    sortedActivity.append(wrappedActivity);
+                } else if tags != nil && (tags!.contains(word1) || tags!.contains(word2)) {
+                    //TODO: make tags lowercase
+                    sortedActivity.append(wrappedActivity);
+                } else if location != nil && (location!.lowercased().contains(word1) || location!.lowercased().contains(word2)) {
+                    //sortedActivity.append(wrappedActivity);
+                } else {
+                    wrappedActivity.decrementPriority();
+                }
+            }
+            
+            sortedActivity.sort(by: compare)
+            for activity in sortedActivity {
+                filteredActivities.append(activity.activity);
+            }
+            /*
+             if title.contains(text) {
+                 filteredActivities.append(activity);
+             } else if description != nil && description!.lowercased().contains(text) {
+                 filteredActivities.append(activity);
+             } else if tags != nil && tags!.contains(text) {
+                 //TODO: make tags lowercase
+                 filteredActivities.append(activity);
+             } else if location != nil && location!.lowercased().contains(text) {
+                 filteredActivities.append(activity);
+             }
+             */
+                
                     
             filtered = true;
             self.listOfActivities.reloadData();
